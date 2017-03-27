1064b360ec3612114b3d8f000a5f26a0e7b2c961
Staremang <Staremang@ya.ru>
2017-03-27 21:05:57 +0700

    fix#2

--- js-source/main.js
+++ js-source/main.js
@@ -1,8 +1,10 @@
 
 var setDate = function () {
-	var t = [],
-		l = -1, r = -1,
-		y, m;
+	var t = [], // Начальный массив лет
+		l = -1, // Счетчик лет
+		r = -1, // Счетчик месяцев
+		y, // Текущий год в массиве
+		m; // Текущий месяц в массиве
 	
 	for (var i = 0; i < document.querySelectorAll('.portfolio-grid__item[data-date]').length; i++) {
 //		y = Math.floor( Date.parse(document.querySelectorAll('.portfolio-grid__item[data-date]')[i].getAttribute('data-date'))/(1000*60*60*24*365)) + 1970;
@@ -26,20 +28,21 @@ var setDate = function () {
 		}
 		t[l].months[r].el[t[l].months[r].el.length] = document.querySelectorAll('.portfolio-grid__item[data-date]')[i];
 	}
-	console.log(t)
 	return t;
 }
 function setScale () {
-	var el,
-		elM,
-		date = new setDate(),
+	var el, // Маркер лет
+		elM, // Маркер месяцев
+		date = new setDate(), // Массив с годами и месяцами
 		h,
-		headerH = document.querySelector('.header').offsetHeight,
-		gridHeight = document.querySelector('.portfolio-grid').offsetHeight,
-		scaleBlock = document.querySelector('.scale'),
-		scroll;
-	scaleBlock.innerHTML = '';
-	var mouse = document.createElement('div');
+		headerH = document.querySelector('.header').offsetHeight, // Высота шапки (она переменная)
+		gridHeight = document.querySelector('.portfolio-grid').offsetHeight, // Высота всего блока с элементами портфолио
+		scaleBlock = document.querySelector('.scale'), // Блок с маркерами
+		scroll,
+		mouse = document.createElement('div'); // Маркер позиции мыши
+	
+	scaleBlock.innerHTML = ''; // Обнуляем, чтобы при resize там ничего не было
+	
 	mouse.classList.add('scale__mouse');
 	scaleBlock.appendChild(mouse);
 	
@@ -83,9 +86,12 @@ function setScale () {
 	}
 	
 	
-	
+	var c = 0; // Счетчик количества табличек, ушедших за пределы блока
 	date.forEach(function(item, i, arr) {
 		var itemTop;
+		/*
+			Идем по массиву годов и вставляем таблички с годом
+		*/
 		el = document.createElement('div');
 		el.classList.add('scale__item');
 		el.innerHTML = item.year;
@@ -94,12 +100,19 @@ function setScale () {
 		*/
 		itemTop = (document.querySelector('.scale').offsetHeight) * (item.months[0].el[0].offsetTop) / (gridHeight - document.documentElement.clientHeight + headerH);
 		if (itemTop >= document.querySelector('.scale').offsetHeight) {
-			itemTop = document.querySelector('.scale').offsetHeight - 20;
+			/*
+				20 - высота таблички с годом
+				22 - высота таблички с отступом
+			*/
+			itemTop = document.querySelector('.scale').offsetHeight - (20 + 22 * c);
+			c++;
 		}
 		/* 
 			В пропорциях относительно .scale
+			
+		itemTop = (document.querySelector('.scale').offsetHeight - 20) * (date[i].el[0].offsetTop)/(date[date.length - 1].el[0].offsetTop);
+		
 		*/
-//		itemTop = (document.querySelector('.scale').offsetHeight - 20) * (date[i].el[0].offsetTop)/(date[date.length - 1].el[0].offsetTop);
 		
 		el.style.transform = 'translate(0, ' + itemTop + 'px)';
 		
@@ -123,7 +136,9 @@ function setScale () {
 		scaleBlock.appendChild(el);
 		
 		item.months.forEach(function(item, i, arr) {
-//			var itemTop;
+			/*
+				Идем по массиву месяцев и вставляем указатели
+			*/
 			elM = document.createElement('div');
 			elM.classList.add('scale__item_m');
 			/* 
@@ -131,13 +146,14 @@ function setScale () {
 			*/
 			itemTop = (document.querySelector('.scale').offsetHeight) * (item.el[0].offsetTop) / (gridHeight - document.documentElement.clientHeight + headerH);
 
-//			if (itemTop >= document.querySelector('.scale').offsetHeight) {
-//				itemTop = document.querySelector('.scale').offsetHeight - 20;
-//			}
+			if (itemTop >= document.querySelector('.scale').offsetHeight) {
+				itemTop = document.querySelector('.scale').offsetHeight - 20;
+			}
 			/* 
 				В пропорциях относительно .scale
+				
+			itemTop = (document.querySelector('.scale').offsetHeight - 20) * (date[i].el[0].offsetTop)/(date[date.length - 1].el[0].offsetTop);
 			*/
-	//		itemTop = (document.querySelector('.scale').offsetHeight - 20) * (date[i].el[0].offsetTop)/(date[date.length - 1].el[0].offsetTop);
 
 			elM.style.transform = 'translate(0, ' + itemTop + 'px)';
 			scaleBlock.appendChild(elM);
@@ -148,23 +164,32 @@ function setScale () {
 window.onload = window.onresize = function () {
 	var firstHeight;
 	if (document.documentElement.clientWidth <= 768) {
-		document.querySelector('.header__mobile').classList.add('hide');
-		firstHeight = 2 * document.documentElement.clientWidth * 1070 / 1920;
 		
-		if (document.querySelector('.first__grid-top')) {
-			document.querySelector('.first__grid-top').style.height = firstHeight / 2 + 'px';
+		if (document.querySelector('.header__mobile')) {
+			document.querySelector('.header__mobile').classList.add('hide');
 		}
-		
-		if (document.querySelector('.first__grid-left')) {
+		if (document.querySelector('.first')) {
+			
+			firstHeight = 2 * document.documentElement.clientWidth * 1070 / 1920;
+			
+			document.querySelector('.first__grid-top').style.height = firstHeight / 2 + 'px';
 			document.querySelector('.first__grid-left').style.height = firstHeight + 'px';
 		}
+		
 	} else {
-		document.querySelector('.header__mobile').classList.remove('hide');
-		firstHeight = document.documentElement.clientWidth * 1070 / 1920;
+		
+		if(document.querySelector('.portfolio')) {
+			setScale();
+		}
+		if (document.querySelector('.header__mobile')) {
+			document.querySelector('.header__mobile').classList.remove('hide');
+		}
 		
 		if (document.querySelector('.first')) {
+			firstHeight = document.documentElement.clientWidth * 1070 / 1920;
 			document.querySelector('.first').style.height = firstHeight + 'px';
 		}
+		
 	}
 	
 	for (var i = 0; i < document.querySelectorAll('.first__grid-left .first__item').length; i++) {
@@ -173,27 +198,34 @@ window.onload = window.onresize = function () {
 	for (var i = 0; i < document.querySelectorAll('.first__grid-top .first__item').length; i++) {
 		document.querySelectorAll('.first__grid-top .first__item')[i].style.height = firstHeight / 2 + 'px';
 	}
-	if(document.querySelector('.portfolio')) {
-		setScale();
-	}
-//	setDate();
-//	setScale();
 }
 $(document).ready(function () {
+	var contactsButton = $('a[href*="#contacts"]');
+	
+	if (document.documentElement.clientWidth <= 768) {
+		$('.header__item').click(function () {
+			$('.header__mobile').addClass('hide');
+		})
+	}
 	if(document.querySelector('.popup-contacts')) {
-		$('a[href*="#contacts"]').click(function () {
+		
+		contactsButton.click(function () {
+			contactsButton.addClass('active');
 			$('.popup-contacts').fadeIn();
-			$(this).addClass('active');
-//			$('.popup-contacts').addClass('fadeIn');
 		})
 		$('.contacts__btn-close').click(function () {
-			$('a[href="#contacts"]').removeClass('active');
+			contactsButton.removeClass('active');
 			$('.popup-contacts').fadeOut();
 		})
+		
 	} else {
-		$('a[href*="#contacts"]').anchor({
+		contactsButton.anchor({
 			transitionDuration: 1000
 		});
+		contactsButton.click(function () {
+			$('.header__mobile').toggleClass('hide');
+		})
+		
 	}
 	$('a[href="#reviews"]').anchor({
 		transitionDuration: 1000
@@ -201,12 +233,6 @@ $(document).ready(function () {
 	$('.header__btn').click(function () {
 		$('.header__mobile').toggleClass('hide');
 	})
-	if (document.documentElement.clientWidth <= 768) {
-		$('.header__item').click(function () {
-			console.log('цепляется ')
-			$('.header__mobile').addClass('hide');
-		})
-	}
 	$('.reviews__container').owlCarousel({
 		loop:true,
 		margin:10,
@@ -263,14 +289,12 @@ $(document).ready(function () {
 		}
 	});
 	$(".services__col").mousemove(function(e) {
-//		console.log('Событие наведения');
 		var offset = $(this).children().offset();
 		var relativeX = (e.pageX - offset.left - $(this).children().innerWidth()/2)/($(this).children().innerWidth()/2);
 		var relativeY = (e.pageY - offset.top - $(this).children().innerHeight()/2)/($(this).children().innerHeight()/2),
 			t = 2;
 		
 		$(this).children().css('transform', 'rotateX(' + -relativeY * t + 'deg) rotateY(' + relativeX * t + 'deg)')
-//		console.log("X: " + relativeX + "  Y: " + relativeY, $(this).innerWidth());
 	});
 	$(".services__col").mouseover(function(e) {
 		$(this).children().css('transition', '')
